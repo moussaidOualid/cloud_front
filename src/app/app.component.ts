03e804ddf08dfbb4786e5b98c98a330f3fa8d07f
moussaidOualid <moussaid.oualid@gmail.com>
2019-12-31 01:20:51 +0100

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front';
+  results : any ;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    // Making the HTTP Request
+    this.http
+     .get('http://35.194.28.13:8088/users/1')
+     .subscribe(data => {
+       this.title = data['nom'];
+      // this.title=data['results']['nom'];
+       console.log(data);
+     })
+  }
+ 
 }
